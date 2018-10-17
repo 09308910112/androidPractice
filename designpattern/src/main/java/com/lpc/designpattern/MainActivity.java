68e7ae0617d22d0056761aa759f4522cd443aec7
lpc <liupengcheng2014@gmail.com>
2018-10-17 09:24:36 +0800

    装饰者设计模式

--- designpattern/src/main/java/com/lpc/designpattern/MainActivity.java
+++ designpattern/src/main/java/com/lpc/designpattern/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.lpc.designpattern.decorator.first.DecoratorPatternActivity01;
 import com.lpc.designpattern.strategy.second.StrategyPatternActivity02;
 import com.lpc.designpattern.strategy.first.StrategyPatternActivity;
 
@@ -26,4 +27,9 @@ public class MainActivity extends AppCompatActivity {
     public void strategy02(View view) {
         startActivity(new Intent(this, StrategyPatternActivity02.class));
     }
+
+    public void decorate01(View view) {
+        startActivity(new Intent(this, DecoratorPatternActivity01.class));
+    }
+
 }
